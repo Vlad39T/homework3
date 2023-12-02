ab2c789b91f1e82aa1714851f6568440dfb7b89d
Vlad39T <aavgustin39@gmail.com>
2023-12-02 21:11:02 +0200

    Homework24

---
+++ src/main/java/homework24/Fine.java
@@ -0,0 +1,22 @@
+package homework24;
+
+public class Fine {
+    private String name;
+
+    public Fine(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return  name ;
+    }
+}
