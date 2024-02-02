cf8537ccb383cf51db2f8c95e4ab82765c0ceb5a
Vlad Tkachenko <aavgustin39@gmail.com>
2024-02-02 20:16:59 +0200

    Homework8

---
+++ src/main/java/Web/Lesson1/Homework_Lesson1/HW9/plus.js
@@ -0,0 +1,13 @@
+function addBlock() {
+    let clBlock = document.createElement('div');
+    clBlock.classList.add('colorB');
+    clBlock.style.backgroundColor = randomColors();
+    clBlock.onclick = function () {
+        clBlock.remove(clBlock);
+    };
+document.getElementById('block').appendChild(clBlock);
+}
+
+function randomColors() {
+    return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
